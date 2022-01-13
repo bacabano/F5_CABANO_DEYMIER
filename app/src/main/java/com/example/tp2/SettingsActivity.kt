552e369b70b12b84d908eb807352f3b7de281ce7
Bastien Cabano <bastien.cabano@gmail.com>
2022-01-13 23:03:17 +0100

    Update settings and hint search button

--- app/src/main/java/com/example/tp2/SettingsActivity.kt
+++ app/src/main/java/com/example/tp2/SettingsActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatDelegate
 
+
 class SettingsActivity : AppCompatActivity() {
+
+
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +17,16 @@ class SettingsActivity : AppCompatActivity() {
         supportActionBar?.title = "Settings"
         setContentView(R.layout.activity_settings)
 
+
         //DARK MODE
         val btn = findViewById<Switch>(R.id.switch1)
+
+        //Handle the state of the switch button
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            btn.isChecked = true
+        }
+
+        //Switch themes
         btn.setOnCheckedChangeListener { _, isChecked ->
             if (btn.isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -24,4 +35,5 @@ class SettingsActivity : AppCompatActivity() {
             }
         }
     }
+
 }
